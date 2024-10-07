4595430e3e8bd71000f1087984faadbd70eab2b2
tanselbay1 <tanselbay1@hotmail.com>
2024-10-07 21:16:04 +0700

    add header file

---
+++ minitalk.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanselbayraktaroglu <tanselbayraktarogl    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 19:33:18 by tanselbayra       #+#    #+#             */
+/*   Updated: 2024/09/30 20:54:30 by tanselbayra      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+#define MINITALK_H
+
+#include "libft.h"
+#include "ft_printf.h"
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#endif
