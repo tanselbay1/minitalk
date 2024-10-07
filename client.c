23bc88dcb133c1b6f533449bb5dce173e7312e44
tanselbay1 <tanselbay1@hotmail.com>
2024-10-07 21:15:01 +0700

    create client

---
+++ client.c
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tanselbayraktaroglu <tanselbayraktarogl    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/30 19:31:19 by tanselbayra       #+#    #+#             */
+/*   Updated: 2024/10/04 16:26:51 by tanselbayra      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minitalk.h"
+
+void send_signals(int pid, char *message)
+{
+   int letter;
+   int i;
+
+   letter = 0;
+   while (message[letter])
+   {
+      i = -1;
+      while (++i < 8)
+      {
+         if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 0)
+            kill(pid, SIGUSR1);
+         else if (((unsigned char)(message[letter] >> (7 - i)) & 1) == 1)
+            kill(pid, SIGUSR2);
+         usleep(50);
+      }
+      letter++;
+   }
+   i = 0;
+   while (i++ < 8)
+   {
+      kill(pid, SIGUSR1);
+      usleep(50);
+   }
+}
+
+int main(int argc, char **argv)
+{
+   char *message;
+   int pid;
+
+   if (argc == 3)
+   {
+      pid = ft_atoi(argv[1]);
+      if (!pid)
+      {
+         ft_printf("[ERROR]. Wrong arg");
+         return (0);
+      }
+      message = argv[2];
+      if (message[0] == 0)
+      {
+         ft_printf("There is no message!");
+         return (0);
+      }
+      send_signals(pid, message);
+   }
+   else
+      ft_printf("[ERROR]. Format: ./client <pid> <message>");
+   return (0);
+}
